b900fbd95891575246e6bc23aeffd51c3df94b04
mikkelthomsen1 <mikkel.thomsen18@gmail.com>
2023-02-06 10:53:58 +0100

    l

--- searchopg/src/searchopg/Main.java
+++ searchopg/src/searchopg/Main.java
@@ -4,17 +4,40 @@ import java.util.Arrays;
 public class Main {
 	
  public static void main(String[] args) {
-	 int[] tabel = {5,1,4,3,7,9,8,2,6};
-	 Arrays.sort(tabel);
+	 int[] table = {5,1,6,3,7,11,8,2,6,10,100,12}; //array of int search
+	 Arrays.sort(table); //sort array
 	 
-	 for (int i = 0; i < tabel.length; i++)
-		 System.out.print(tabel[i]+",");
-	 
-	 System.out.println("");
-	 System.out.println("det søgte element er er placeret på [" + Arrays.binarySearch(tabel,5)+"]");
-	 //1234
-	 //123
-	 //test
+	 for (int i = 0; i < table.length; i++) { //print array
+		System.out.print(table[i]+",");
+		}
+		System.out.println();	
+		System.out.println(Arrays.toString(table));
+	 	
+	int index = Arrays.binarySearch(table,112); 
+
+	 if ( index < 0 ) { //check if element exists in array
+		System.out.println("element does not exist in array");
+	 } else System.out.println("element is located at: [" + index  +"]");
+
+
+
+
+
+
+	 String[] teachers = {"Xiaolei","Jan","Flemming"}; //array of strings search
+	 Arrays.sort(teachers);
+
+	 for (int i = 0; i < teachers.length; i++) //Print array
+	  System.out.print(teachers[i]+",");
+	
+	System.out.println("");	
+
+	int index1 = Arrays.binarySearch(teachers, ""); //sort array alphabetical
+	
+	if ( index1 < 0 ) { //check if element exists in array
+			System.out.println("element does not exist in array"); 
+	} else System.out.println("element is located at: [" + index1  +"]");
+	
  }
  
 }
